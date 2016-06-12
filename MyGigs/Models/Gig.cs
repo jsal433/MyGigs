e95f4a85c0e0ce87493ae479d8537765a315adb3
Joshua Salmon <joshs.23@hotmail.com>
2016-06-12 23:32:42 +1200

    Implement saving a gig

--- MyGigs/Models/Gig.cs
+++ MyGigs/Models/Gig.cs
@@ -7,8 +7,10 @@ namespace MyGigs.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -16,7 +18,9 @@ namespace MyGigs.Models
         [StringLength(255)]
         public String Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
