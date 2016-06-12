adafed3d9c35ef5cfc3a1e08c4d50d94f01b17c8
jsal433 <jsal433@aucklanduni.ac.nz>
2016-06-12 18:09:10 +1200

    Initial commit

---
+++ MyGigs/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyGigs.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
---
+++ MyGigs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGigs.Startup))]
+namespace MyGigs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
