44404968d9f918958d9bfb95e1397c9f68c9381d
Joshua Salmon <joshs.23@hotmail.com>
2016-06-24 19:54:03 +1200

    Implement view gigs you are attending

--- MyGigs/Controllers/GigsController.cs
+++ MyGigs/Controllers/GigsController.cs
@@ -3,6 +3,7 @@ using MyGigs.Models;
 using MyGigs.ViewModels;
 using System.Linq;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MyGigs.Controllers
 {
@@ -13,6 +14,27 @@ namespace MyGigs.Controllers
         public GigsController()
         {
             _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var gigs = _context.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Gig)
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .ToList();
+
+            var viewModel = new GigsViewModel()
+            {
+                UpcomingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         [Authorize]
