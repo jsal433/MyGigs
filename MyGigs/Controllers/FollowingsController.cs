a9cd22737cecd4308a06b046f2d9d4af69b60a68
Joshua Salmon <joshs.23@hotmail.com>
2016-06-24 18:33:01 +1200

    Added Following functionality (users can now follow artists)

---
+++ MyGigs/Controllers/FollowingsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using MyGigs.Dtos;
+using MyGigs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyGigs.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FollowerId == dto.FolloweeId))
+                return BadRequest("Following already exists");
+
+            var following = new Following
+            {
+                FolloweeId = dto.FolloweeId,
+                FollowerId = userId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ MyGigs/Dtos/FollowingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MyGigs.Dtos
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
