f454d9113fa405defb04fd07d7705b4cea100fc2
Joshua Salmon <joshs.23@hotmail.com>
2016-07-06 12:32:26 +1200

    Implement Gig details

--- MyGigs/Controllers/GigsController.cs
+++ MyGigs/Controllers/GigsController.cs
@@ -17,6 +17,35 @@ namespace MyGigs.Controllers
             _context = new ApplicationDbContext();
         }
 
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Genre)
+                .Include(g => g.Artist)
+                .SingleOrDefault(g => g.Id == 9);
+
+            if (gig == null)
+                return HttpNotFound();
+
+            var viewModel = new GigDetailsViewModel()
+            {
+                Gig = gig
+            };
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                viewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+
+                viewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+
+            return View(viewModel);
+        }
+
         [Authorize]
         public ActionResult Mine()
         {
